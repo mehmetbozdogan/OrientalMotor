f50fcdc3b57054089d72e7fc2e42d41574f19459
mehmetbozdogan <33389059+mehmetbozdogan@users.noreply.github.com>
2017-11-20 09:50:47 +0200

    Update st.cmd

--- orientalMotor/iocBoot/iocoriental/st.cmd
+++ orientalMotor/iocBoot/iocoriental/st.cmd
@@ -15,15 +15,15 @@ oriental_registerRecordDeviceDriver(pdbbase)
 ## Load record instances
 dbLoadTemplate "db/template.substitutions"
 
-   drvAsynSerialPortConfigure("BL-C001-ORIENTAL","/dev/ttyS0")
-   asynSetOption("BL-C001-ORIENTAL",0,"baud","9600")
-   asynSetOption("BL-C001-ORIENTAL",0,"bits","8")
-   asynSetOption("BL-C001-ORIENTAL",0,"parity","even")
-   asynSetOption("BL-C001-ORIENTAL",0,"stop","1")
-   asynSetOption("BL-C001-ORIENTAL",0,"clocal","N")
-   asynSetOption("BL-C001-ORIENTAL",0,"crtscts","N")
-   asynSetTraceMask("BL-C001-ORIENTAL",-1,0x9)
-   asynSetTraceIOMask("BL-C001-ORIENTAL",-1,0x4)
+   drvAsynSerialPortConfigure("SYS-SEC-ORIENTAL","/dev/ttyS0")
+   asynSetOption("SYS-SEC-ORIENTAL",0,"baud","9600")
+   asynSetOption("SYS-SEC-ORIENTAL",0,"bits","8")
+   asynSetOption("SYS-SEC-ORIENTAL",0,"parity","even")
+   asynSetOption("SYS-SEC-ORIENTAL",0,"stop","1")
+   asynSetOption("SYS-SEC-ORIENTAL",0,"clocal","N")
+   asynSetOption("SYS-SEC-ORIENTAL",0,"crtscts","N")
+   asynSetTraceMask("SYS-SEC-ORIENTAL",-1,0x9)
+   asynSetTraceIOMask("SYS-SEC-ORIENTAL",-1,0x4)
 
 iocInit()
 startPVAServer
